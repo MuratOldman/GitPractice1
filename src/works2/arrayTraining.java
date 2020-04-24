1e95733641164ec2195b6812c810a0c105894fcc
MuratOldman <muratkara15102@gmail.com>
2020-04-24 14:31:57 -0400

    add arrayTraining.java

---
+++ src/works2/arrayTraining.java
@@ -0,0 +1,11 @@
+package works2;
+
+import java.util.Arrays;
+
+public class arrayTraining {
+    public static void main(String[] args) {
+        int[]arr={6,4,7,3,8,2,0,6};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
